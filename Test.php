a43ef41f93e1546d2b53725698523d9167d73fd6
Rich <Rich@Rich-PC.home>
2015-09-03 10:29:03 -0400

    This is just a test for setting up git in netbeans.

---
+++ Test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+This is in no way related to the project. This is a simple test push
+to make sure that Net Beans is properly working with Git.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo("Hello World!");
+        ?>
+    </body>
+</html>
